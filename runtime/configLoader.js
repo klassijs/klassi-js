382a71250cd4fcb1806e3577417646f46c27cab2
larryg01 <larryg01@users.noreply.github.com>
2019-02-25 15:49:48 +0000

    removing some personal data

--- runtime/configLoader.js
+++ runtime/configLoader.js
@@ -3,7 +3,7 @@
  Created by Larry Goddard
  */
 /**
- Copyright © klassitech 2019 - Larry Goddard <larryg@klassitech.co.uk>
+ Copyright © klassitech 2016 - Larry Goddard <larryg@klassitech.co.uk>
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
