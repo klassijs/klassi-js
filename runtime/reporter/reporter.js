29fc23c391165589d1e16c6a4f7a900490576758
LarryG <larryg01@users.noreply.github.com>
2020-03-04 09:00:27 +0000

    cleaned up example test and reporter (#59)

--- runtime/reporter/reporter.js
+++ runtime/reporter/reporter.js
@@ -21,18 +21,10 @@
 
 const fs = require('fs-extra');
 const path = require('path');
-const reporter = require('multiple-cucumber-html-reporter');
-const useragent = require('ua-parser-js');
+const reporter = require('cucumber-html-reporter');
 const getRemote = require('../getRemote');
 
 let remoteService = getRemote(global.settings.remoteService);
-let uastring = fs.readFileSync('../../projects/shared-objects/docs/userAgent.txt', 'utf8');
-// let uastring = require('../../projects/shared-objects/docs/userAgent.txt'); // TODO: change location to be global
-let parser = new useragent(uastring);
-let reportOptions;
-// let { metadata } = require('./metaData');
-
-console.log(parser.getResult());
 
 module.exports = {
   reporter: function() {
@@ -40,107 +32,32 @@ module.exports = {
     if (global.paths.reports && fs.existsSync(global.paths.reports)) {
       global.endDateTime = helpers.getEndDateTime();
 
-      // Single reporter
-
-      // let reportOptions = {
-      //   theme: "bootstrap",
-      //   jsonFile: path.resolve(
-      //     global.paths.reports,
-      //     projectName + " " + global.settings.reportName + "-" + date + ".json"
-      //   ),
-      //   output: path.resolve(
-      //     global.paths.reports,
-      //     projectName + " " + global.settings.reportName + "-" + date + ".html"
-      //   ),
-      //   reportSuiteAsScenarios: true,
-      //   launchReport: !global.settings.disableReport,
-      //   ignoreBadJsonFile: true,
-      //   metadata: {
-      //     "Test Started": startDateTime,
-      //     "Test Completion": endDateTime,
-      //     Platform: process.platform,
-      //     Environment: global.envConfig.envName,
-      //     Browser: global.settings.remoteConfig || global.browserName,
-      //     Executed:
-      //       remoteService && remoteService.type === "browserstack"
-      //         ? "Remote"
-      //         : "Local"
-      //   },
-      //   brandTitle: projectReportName + " " + reportName + "-" + date,
-      //   name: projectReportName
-      // };
-
-      // TODO: WIP for new style reporter
-      reportOptions = {
-        jsonDir: path.resolve(global.paths.reports),
-
-        reportPath: path.resolve(
-          global.paths.reports, browserName + '-' + date
-          // projectName + ' ' + global.settings.reportName + '-' + date
+      let reportOptions = {
+        theme: 'bootstrap',
+        jsonFile: path.resolve(
+          global.paths.reports,
+          projectName + ' ' + global.reportName + '-' + date + '.json'
         ),
-        // saveCollectedJSON: true,
-
-        disableLog: false,
-        pageTitle: 'Automation Report',
-        reportName: 'Test Automation Report' + '-' + date,
-        openReportInBrowser: !global.settings.disableReport,
-
-        customMetadata: true,
-        // metadata: metadata, // TODO: WIP for the new reporter
-        metadata: [
-          {
-            name: 'Browser',
-            value: parser.getBrowser().name + ' ' + parser.getBrowser().version
-          },
-          {
-            name: 'OS',
-            value: parser.getOS().name + ' ' + parser.getOS().version
-          },
-          {
-            name: 'Device',
-            value:
-              remoteService && remoteService.type === 'browserstack'
-                ? 'Remote'
-                : 'Local'
-          },
-          {
-            name: 'Date',
-            value: helpers.getCurrentDateTime()
-          }
-        ],
-        displayDuration: true,
-        customData: {
-          title: 'Test Run Info',
-          data: [
-            {
-              label: 'Project',
-              value: 'Klassi Automation'
-            },
-            {
-              label: 'Environment',
-              value: global.envConfig.envName
-            },
-            {
-              label: 'Platform',
-              value: process.platform
-            },
-            {
-              label: 'Executed',
-              value:
-                remoteService && remoteService.type === 'browserstack'
-                  ? 'Remote'
-                  : 'Local'
-            },
-            {
-              label: 'Execution Start Time',
-              value: startDateTime
-            },
-            {
-              label: 'Execution End Time',
-              value: endDateTime
-            }
-          ]
-        }
+        output: path.resolve(
+          global.paths.reports,
+          projectName + ' ' + global.reportName + '-' + date + '.html'
+        ),
+        reportSuiteAsScenarios: true,
+        launchReport: !global.settings.disableReport,
+        ignoreBadJsonFile: true,
+        metadata: {
+          'Test Started': startDateTime,
+          'Test Completion': endDateTime,
+          Platform: process.platform,
+          Environment: global.envConfig.envName,
+          Browser: global.settings.remoteConfig || global.browserName,
+          Executed:
+            remoteService && remoteService.type === 'browserstack'
+              ? 'Remote'
+              : 'Local'
+        },
+        brandTitle: projectReportName + ' ' + reportName + '-' + date,
+        name: projectReportName
       };
       browser.pause(DELAY_2s).then(async() => {
         await reporter.generate(reportOptions);
