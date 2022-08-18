00229c2ee5b9ebccc335e9eb5fb8255d37e2e637
Carlos Bermejo Pérez <carlosbermejo-oup@gmail.com>
2022-08-18 16:35:11 +0200

    Fixed UTAM Google test

--- shared-objects/searchData.js
+++ shared-objects/searchData.js
@@ -2,7 +2,7 @@ module.exports = {
   elem: {
     searchInput: '[name="q"]',
     resultLink: 'div#links',
-    searchBtn: 'input.search__button.js-search-button',
+    searchBtn: 'button[aria-label="Search"]',
     leftBadge:
       'body > div.site-wrapper.js-site-wrapper > div.badge-link.badge-link--serp.ddg-extension-hide.js-badge-link',
   },
