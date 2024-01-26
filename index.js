242deba6b9893d9156aa557737573e716c59673e
LarryG <larryg@klassitech.co.uk>
2024-01-26 17:07:39 +0000

    chore(): updating code

--- index.js
+++ index.js
@@ -126,6 +126,7 @@ program
     collectPaths,
     []
   )
+  .option('--updateBaselineImage', 'automatically update the baseline image after a failed comparison')
   .option('--browserOpen', 'keep the browser open after each scenario. defaults to false', false)
   .option('--wdProtocol', 'the switch to change the browser option from devtools to webdriver', false)
   .option('--dlink', 'the switch for projects with their test suite, within a Test folder of the repo', false)
